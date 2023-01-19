cdbbd74cc75976843eeff16d2db906bf6a43da5a
unknown <michael.larsen91@gmail.com>
2023-01-18 23:34:47 -0800

    added files to repo

---
+++ requirements.bat
@@ -0,0 +1,13 @@
+pip install wolframalpha
+pip install wikipedia
+pip install SpeechRecognition
+pip install ecapture
+pip install twilio
+pip install beautifulsoup4
+pip install pyttsx3
+pip install requests
+pip install pyjokes
+pip install winshell
+pip install feedparser
+pip install clint
+pip install pyaudio
